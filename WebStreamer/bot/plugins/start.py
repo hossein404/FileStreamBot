2384691bcb09f3eab1092a4aab1d5b30ca7cc921
Taha <131471887+iaMaST3R@users.noreply.github.com>
2025-07-28 11:49:21 +0300

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -4,14 +4,20 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 from WebStreamer.vars import Var
 from WebStreamer.bot import StreamBot
 from WebStreamer.bot.database import add_or_update_user, is_user_authorized, set_user_lang
-from WebStreamer.bot.i18n import get_i18n_texts
+from WebStreamer.bot.i18n import get_i18n_texts, translations
 
 @StreamBot.on_message(filters.command(["start", "help"]) & filters.private)
 async def start(bot: Client, m: Message):
     if not await is_user_authorized(m.from_user.id):
-        lang_texts = await get_i18n_texts(m.from_user.id)
-        return await m.reply(lang_texts.get("NOT_AUTHORIZED"), quote=True)
+        # For unauthorized users, send a message in both languages
+        bilingual_message = (
+            f"{translations['fa'].get('NOT_AUTHORIZED')}\n\n"
+            "------\n\n"
+            f"{translations['en'].get('NOT_AUTHORIZED')}"
+        )
+        return await m.reply(bilingual_message, quote=True)
 
+    # If authorized, proceed with the normal flow
     await add_or_update_user(
         user_id=m.from_user.id,
         first_name=m.from_user.first_name,
@@ -45,6 +51,15 @@ async def start(bot: Client, m: Message):
 
 @StreamBot.on_callback_query(filters.regex("^set_lang_"))
 async def language_setter(bot: Client, query: CallbackQuery):
+    if not await is_user_authorized(query.from_user.id):
+        # For unauthorized users, send a message in both languages
+        bilingual_message = (
+            f"{translations['fa'].get('NOT_AUTHORIZED')}\n\n"
+            "------\n\n"
+            f"{translations['en'].get('NOT_AUTHORIZED')}"
+        )
+        return await query.answer(bilingual_message, show_alert=True)
+    
     lang_code = query.data.split("_")[-1]
     await set_user_lang(query.from_user.id, lang_code)
     
@@ -68,5 +83,5 @@ async def language_setter(bot: Client, query: CallbackQuery):
 
     try:
         await query.message.edit_text(start_text, reply_markup=reply_markup, disable_web_page_preview=True)
-    except: # MessageNotModified
+    except: # Catches MessageNotModified error
         pass
